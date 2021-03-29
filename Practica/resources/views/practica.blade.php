b481e8731e5cb5abc99b688f36a5513adfa111c2
Angélica <angelicasalaicesarriola@gmail.com>
2021-03-29 00:56:06 -0700

    Práctica Bootstrap
    Práctica Bootstrap Clase 23/03/2021

---
+++ Practica/resources/views/practica.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('/mediaquerys.css')}}">
+    <title>Práctica</title>
+</head>
+<body>
+    <div id="vista1" style="height: 1000px; margin: 30px;">
+        <div class=" bg-info" style="height: 300px; margin: 30px;">
+        <h2>A</h2>
+        </div>
+        <div class="row" style="height: 150px; margin: 20px;">
+            <div class="col bg-info" style="margin: 10px;"><h2>B</h2></div>
+            <div class="col bg-info" style="margin: 10px;"><h2>C</h2></div>
+            <div class="col bg-info" style="margin: 10px;"><h2>D</h2></div>
+            <div class="col bg-info" style="margin: 10px;"><h2>E</h2></div>
+        </div>
+
+        <div class="row" style="height: 150px; margin: 20px;">
+            <div class="col bg-info" style="margin: 10px;"><h2>F</h2></div>
+            <div class="col bg-info" style="margin: 10px;"><h2>G</h2></div>
+        </div>
+        <div style="height: 40px; margin: 30px;">
+            <div class=" bg-info" style=" width:74%; float:left;"><h2>H</h2></div>
+            <div class=" bg-info" style=" width:25%; float:left;margin-left: 10px;"><h2>I</h2></div>
+        </div>
+        <br>
+        <div class="row"style="height: 100px; margin: 50px;">
+            <div class="col-5 bg-info"><h2>J</h2></div>
+            <div class="col-2 "></div>
+            <div class="col-5 bg-info"><h2>K</h2></div>
+        </div>
+
+    </div>
+
+    <div id="vista2">
+        <div class=" bg-info" style="height: 250px; margin: 30px;">
+            <h2>L</h2>
+        </div>
+        <div class="row" style="height: 200px; margin: 20px;">
+            <div class="col bg-info" style=" margin: 10px;"><h2>B</h2></div>
+            <div class="col  bg-info" style=" margin: 10px;"><h2>C</h2></div>
+            <div class="col  bg-info" style=" margin: 10px;"><h2>D</h2></div>
+        </div>
+        <div class="row " style="height: 200px; margin: 20px;">
+            <div class="col bg-info" style=" margin: 10px;"><h2>F</h2></div>
+            <div class="col bg-info" style=" margin: 10px;"><h2>G</h2></div>
+        </div>
+
+        <div class="row bg-info" style="height: 60px; margin: 30px;"><h2>H</h2></div>
+        <div class="row bg-info" style="height: 60px; margin: 30px;"><h2>I</h2></div>
+        <div class="row bg-info" style="height: 60px; margin: 30px;"><h2>J</h2></div>
+        <div class="row bg-info" style="height: 60px; margin: 30px;"><h2>K</h2></div>
+    </div>
+
+    <div id="vista3">
+        <div class=" bg-info" style="height: 200px; margin: 30px;">
+            <h2>L</h2>
+        </div>
+        <div class="row " style="height: 150px; margin: 20px;">
+            <div class="col bg-info" style=" margin: 10px;"><h2>C</h2></div>
+            <div class="col bg-info" style=" margin: 10px;"><h2>D</h2></div>
+        </div>
+        <div class="row bg-info" style="height: 60px; margin: 30px;"><h2>F</h2></div>
+        <div class="row bg-info" style="height: 60px; margin: 30px;"><h2>M</h2></div>
+        <div class="row bg-info" style="height: 40px; margin: 30px;"><h2>H</h2></div>
+        <div class="row bg-info" style="height: 40px; margin: 30px;"><h2>I</h2></div>
+        <div class="row bg-info" style="height: 40px; margin: 30px;"><h2>J</h2></div>
+    </div>
+</body>
+</html>
